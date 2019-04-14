61173f3d408fea104fd49428f386ee664a99ba5c
omarfawzi <omarfawzi96@gmail.com>
2019-04-14 10:52:27 +0200

    fix command

--- src/Command/GeneratorCommand.php
+++ src/Command/GeneratorCommand.php
@@ -8,7 +8,6 @@
 
 namespace ConsumerGenerator\Command;
 
-
 use ConsumerGenerator\Model\ConsumerSkeleton;
 use ConsumerGenerator\Service\GeneratorWrapper;
 use Symfony\Component\Console\Command\Command;
@@ -38,24 +37,61 @@ final class GeneratorCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $generateAnotherConsumer = true;
-
         $consumers = [];
-
         $consumersPath = $this->requiredQuestion($input, $output, new Question('Enter Consumers Path : '));
-
+        $consumersPath .= $this->requiredQuestion(
+            $input,
+            $output,
+            new Question('Enter File Name (without extenstion) : ')
+        );
         while ($generateAnotherConsumer) {
             $consumer = new ConsumerSkeleton();
-            $consumer->setName($this->requiredQuestion($input, $output, new Question('Please Enter The Consumer Name : ')));
-            $consumer->setQueueRoutingKeys($this->requiredQuestion($input, $output, new Question("Please Enter The Consumer Routing Keys separated by ',' : "), true));
-            $consumer->setExchangeType($this->requiredQuestion($input, $output, new Question('Please Enter The Exchange Type : ')));
-            $consumer->setExchangeName($this->requiredQuestion($input, $output, new Question('Please Enter The Exchange Name : ')));
-            $consumer->setQueueName($this->requiredQuestion($input, $output, new Question('Please Enter The Queue Name : ')));
-            $consumer->setCallback($this->requiredQuestion($input, $output, new Question('Please Enter The Path Of the Callback Class : ')));
-            $consumer->setAmqpConsumerType($this->requiredQuestion($input, $output, new ChoiceQuestion('Choose the type of consumer : ', ['consumers', 'batch_consumers'])));
+            $consumer->setName(
+                $this->requiredQuestion($input, $output, new Question('Please Enter The Consumer Name : '))
+            );
+            $consumer->setQueueRoutingKeys(
+                $this->requiredQuestion(
+                    $input,
+                    $output,
+                    new Question("Please Enter The Consumer Routing Keys separated by ',' : "),
+                    true
+                )
+            );
+            $consumer->setExchangeType(
+                $this->requiredQuestion(
+                    $input,
+                    $output,
+                    new ChoiceQuestion('Please Enter The Exchange Type : ', ['topic', 'direct', 'fanout', 'headers'])
+                )
+            );
+            $consumer->setExchangeName(
+                $this->requiredQuestion($input, $output, new Question('Please Enter The Exchange Name : '))
+            );
+            $consumer->setQueueName(
+                $this->requiredQuestion($input, $output, new Question('Please Enter The Queue Name : '))
+            );
+            $consumer->setCallback(
+                $this->requiredQuestion($input, $output, new Question('Please Enter The Path Of the Callback Class : '))
+            );
+            $consumer->setAmqpConsumerType(
+                $this->requiredQuestion(
+                    $input,
+                    $output,
+                    new ChoiceQuestion('Choose the type of consumer : ', ['consumers', 'batch_consumers'])
+                )
+            );
             $consumers[] = $consumer;
-            $generateAnotherConsumer = $this->requiredQuestion($input, $output, new ChoiceQuestion('Generate Another Consumer ?', [true, false]));
+            $answer      = $this->requiredQuestion(
+                $input,
+                $output,
+                new ChoiceQuestion('Generate Another Consumer ?', ['Yes', 'No'])
+            );
+            if ($answer == 'Yes') {
+                $generateAnotherConsumer = true;
+            } else {
+                $generateAnotherConsumer = false;
+            }
         }
-
         $this->wrapper->writeConsumers($consumers, $consumersPath);
 
     }
@@ -65,25 +101,34 @@ final class GeneratorCommand extends Command
      * @param OutputInterface $output
      * @param mixed $question
      * @param bool $arrayAnswer
+     *
      * @return mixed
      */
-    private function requiredQuestion(InputInterface $input, OutputInterface $output, $question, bool $arrayAnswer = false)
-    {
+    private function requiredQuestion(
+        InputInterface $input,
+        OutputInterface $output,
+        $question,
+        bool $arrayAnswer = false
+    ) {
         $helper = $this->getHelper('question');
         $result = null;
         if ($arrayAnswer && $question instanceof Question) {
-            $question->setValidator(function ($answer) use ($output) : array {
-                $removedSpaces = str_replace(' ', '', $answer);
-                if (strpos($answer, ',') !== false) {
-                    return explode(',', $removedSpaces);
-                }
-                return [$removedSpaces];
+            $question->setValidator(
+                function ($answer) use ($output) : array {
+                    $removedSpaces = str_replace(' ', '', $answer);
+                    if (strpos($answer, ',') !== false) {
+                        return explode(',', $removedSpaces);
+                    }
 
-            });
+                    return [$removedSpaces];
+
+                }
+            );
         }
         while (!isset($result)) {
             $result = $helper->ask($input, $output, $question);
         }
+
         return $result;
     }
 }
